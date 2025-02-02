e20ae166ea255ad071e938ed75beebfc2c0f49a8
Christoph Schniedermeier <zdxerr@gmail.com>
2025-02-02 18:29:40 +0100

    Begin day 16.1.

--- day161/src/main.rs
+++ day161/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io::stdin;
+use std::time::Instant;
+use std::{env, fs, str};
+
+const NL: u8 = b'\n';
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let t0 = Instant::now();
+    let input_path = env::args().nth(1).expect("no input path");
+    let map = fs::read(&input_path)?;
+    println!("Read {input_path}.");
+
+    let xlen = map.iter().position(|v| *v == NL).expect("missing new line");
+    println!("{}", str::from_utf8(&map)?);
+    let solution = 0;
+    println!("Solution: {} / Duration: {:.6?}", solution, t0.elapsed());
+    Ok(())
 }
