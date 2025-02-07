214a66d1ce7a2988759de853fca7397b52d7a518
Christoph Schniedermeier <CSchniedermeier@dspace.de>
2025-02-07 16:31:29 +0100

    Day 16.2 try.

--- day162/src/main.rs
+++ day162/src/main.rs
@@ -1,9 +1,7 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::collections::VecDeque;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::error::Error;
 use std::io::stdin;
-use std::ops::IndexMut;
 use std::time::Instant;
 use std::{env, fs, str};
 
@@ -12,7 +10,7 @@ const S: u8 = b'S';
 const E: u8 = b'E';
 const WALL: u8 = b'#';
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Direction {
     N,
     E,
@@ -20,44 +18,70 @@ enum Direction {
     W,
 }
 
-impl Direction {
-    fn left(self: &Direction) -> Direction {
-        match self {
-            Direction::N => Direction::W,
-            Direction::E => Direction::N,
-            Direction::S => Direction::E,
-            Direction::W => Direction::S,
-        }
-    }
-    fn right(self: &Direction) -> Direction {
-        match self {
-            Direction::N => Direction::E,
-            Direction::E => Direction::S,
-            Direction::S => Direction::W,
-            Direction::W => Direction::N,
-        }
-    }
-    fn next(self: &Direction, map: &[u8], xlen: usize, pos: usize) -> Option<usize> {
-        let next_pos = match self {
-            Direction::N => pos.checked_sub(xlen)?,
-            Direction::E => pos.checked_add(1)?,
-            Direction::S => pos.checked_add(xlen)?,
-            Direction::W => pos.checked_sub(1)?,
+#[derive(Debug, Eq, PartialEq)]
+struct State {
+    index: usize,
+    direction: Direction,
+    score: u64,
+}
+
+impl State {
+    fn next(self: &State, map: &[u8], xlen: usize) -> Option<State> {
+        let next_pos = match self.direction {
+            Direction::N => self.index.checked_sub(xlen)?,
+            Direction::E => self.index.checked_add(1)?,
+            Direction::S => self.index.checked_add(xlen)?,
+            Direction::W => self.index.checked_sub(1)?,
         };
         if *map.get(next_pos)? == WALL {
             None
         } else {
-            Some(next_pos)
+            Some(State {
+                index: next_pos,
+                direction: self.direction,
+                score: self.score + 1,
+            })
         }
+    }
+    fn left(self: &State) -> Option<State> {
+        Some(State {
+            index: self.index,
+            direction: match self.direction {
+                Direction::N => Direction::W,
+                Direction::E => Direction::N,
+                Direction::S => Direction::E,
+                Direction::W => Direction::S,
+            },
+            score: self.score + 1000,
+        })
+    }
+    fn right(self: &State) -> Option<State> {
+        Some(State {
+            index: self.index,
+            direction: match self.direction {
+                Direction::N => Direction::E,
+                Direction::E => Direction::S,
+                Direction::S => Direction::W,
+                Direction::W => Direction::N,
+            },
+            score: self.score + 1000,
+        })
     }
 }
 
-#[derive(Debug)]
-struct State {
-    pre: Option<usize>,
-    pos: usize,
-    direction: Direction,
-    score: u64,
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .score
+            .cmp(&self.score)
+            .then_with(|| self.index.cmp(&other.index))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -73,12 +97,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         + 1;
     // println!("{}", str::from_utf8(&map)?);
 
-    let mut queue: VecDeque<State> = VecDeque::new();
-    let mut visited: HashMap<usize, (u64, Vec<usize>)> = HashMap::new();
+    let mut queue: BinaryHeap<State> = BinaryHeap::new();
+    let mut visited: HashMap<(usize, Direction), u64> = HashMap::new();
 
-    queue.push_front(State {
-        pre: None,
-        pos: map
+    queue.push(State {
+        index: map
             .iter()
             .position(|v| *v == S)
             .expect("missing start position in input data"),
@@ -86,67 +109,89 @@ fn main() -> Result<(), Box<dyn Error>> {
         score: 0,
     });
 
-    while let Some(state) = queue.pop_front() {
-        if let Some((last_score, pres)) = visited.get_mut(&state.pos) {
-            if *last_score < state.score {
-                continue;
-            } else if *last_score > state.score {
-                visited.insert(state.pos, (state.score, state.pre.into_iter().collect()));
-            } else {
-                pres.extend(state.pre);
-            }
-        } else {
-            visited.insert(state.pos, (state.score, state.pre.into_iter().collect()));
+    while let Some(state) = queue.pop() {
+        let visit_score = visited
+            .entry((state.index, state.direction))
+            .or_insert(u64::MAX);
+
+        // dbg!(&state, &visit);
+        if state.score >= *visit_score {
+            continue;
         }
 
-        if let Some(pos) = state.direction.left().next(&map, xlen, state.pos) {
-            queue.push_back(State {
-                pre: Some(state.pos),
-                pos,
-                direction: state.direction.left(),
-                score: state.score + 1001,
-            });
-        }
-        if let Some(pos) = state.direction.next(&map, xlen, state.pos) {
-            queue.push_back(State {
-                pre: Some(state.pos),
-                pos,
-                direction: state.direction,
-                score: state.score + 1,
-            });
-        }
-        if let Some(pos) = state.direction.right().next(&map, xlen, state.pos) {
-            queue.push_back(State {
-                pre: Some(state.pos),
-                pos,
-                direction: state.direction.right(),
-                score: state.score + 1001,
-            });
-        }
+        *visit_score = state.score;
+        queue.extend(state.next(&map, xlen));
+        queue.extend(state.left());
+        queue.extend(state.right());
+
         // println!("{}", str::from_utf8(&new_map)?);
         // let mut input: String = String::new();
         // stdin().read_line(&mut input).unwrap();
     }
     let mut queue: Vec<usize> = Vec::new();
-    let mut unique_on_shortest_paths: HashSet<usize> = HashSet::new();
+    let mut shortest_paths: HashSet<usize> = HashSet::new();
     queue.push(
         map.iter()
             .position(|v| *v == E)
             .expect("missing end position in input data"),
     );
-    let mut new_map: Vec<u8> = map.into_iter().collect();
+
+    let mut new_map: Vec<u8> = map.clone();
     while let Some(pos) = queue.pop() {
-        if !unique_on_shortest_paths.insert(pos) {
-            continue;
+        let min = [
+            visited.get(&(pos, Direction::N)),
+            visited.get(&(pos, Direction::E)),
+            visited.get(&(pos, Direction::S)),
+            visited.get(&(pos, Direction::W)),
+        ]
+        .into_iter()
+        .flatten()
+        .min()
+        .unwrap_or(&u64::MAX);
+        dbg!(
+            &queue,
+            pos,
+            visited.get(&(pos, Direction::N)),
+            visited.get(&(pos, Direction::E)),
+            visited.get(&(pos, Direction::S)),
+            visited.get(&(pos, Direction::W)),
+            min,
+        );
+        // let next_pos = match self.direction {
+        //     Direction::N => self.index.checked_sub(xlen)?,
+        //     Direction::E => self.index.checked_add(1)?,
+        //     Direction::S => self.index.checked_add(xlen)?,
+        //     Direction::W => self.index.checked_sub(1)?,
+        // };
+
+        if let Some(score) = visited.get(&(pos, Direction::N)) {
+            if *score == *min {
+                queue.push(pos.checked_add(xlen).unwrap());
+            }
+        }
+        if let Some(score) = visited.get(&(pos, Direction::S)) {
+            if *score == *min {
+                queue.push(pos.checked_sub(xlen).unwrap());
+            }
+        }
+        if let Some(score) = visited.get(&(pos, Direction::E)) {
+            if *score == *min {
+                queue.push(pos.checked_sub(1).unwrap());
+            }
+        }
+        if let Some(score) = visited.get(&(pos, Direction::W)) {
+            if *score == *min {
+                queue.push(pos.checked_add(1).unwrap());
+            }
         }
         new_map[pos] = b'o';
-        let (_, pres) = visited.get(&pos).expect("position not visited");
-        queue.extend(pres);
+        println!("{}", str::from_utf8(&new_map)?);
     }
     println!("{}", str::from_utf8(&new_map)?);
     println!(
         "Solution: {} / Duration: {:.6?}",
-        unique_on_shortest_paths.len(),
+        0,
+        // unique_on_shortest_paths.len(),
         t0.elapsed()
     );
     Ok(())
