2e896852ea99aadd350946ad190093bd1033fd9f
Christoph Schniedermeier <zdxerr@gmail.com>
2025-02-22 21:06:06 +0100

    Day 19.1 and 19.2 solutions.

--- day190/src/main.rs
+++ day190/src/main.rs
@@ -1,6 +1,5 @@
 use std::cell::RefCell;
 use std::error::Error;
-use std::io;
 use std::rc::Rc;
 use std::time::Instant;
 use std::{env, fs, str};
@@ -21,17 +20,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let root = Rc::new(RefCell::new(Trie::new()));
     let mut current_node = Rc::clone(&root);
 
-    let mut current_nodes0: Vec<Rc<RefCell<Trie>>> = vec![Rc::clone(&root)];
-    let mut current_nodes1: Vec<Rc<RefCell<Trie>>> = vec![];
+    let mut current_nodes0: Vec<(u64, Rc<RefCell<Trie>>)> = vec![(1, Rc::clone(&root))];
+    let mut current_nodes1: Vec<(u64, Rc<RefCell<Trie>>)> = vec![];
 
     let mut state = State::ParseTree;
 
-    let mut counter = 0;
+    let mut solution1 = 0;
+    let mut solution2 = 0;
 
     for v in input {
         let index = match (v, &state) {
             (b',', State::ParseTree) => {
-                print!("| ");
                 current_node.borrow_mut().towel = true;
                 current_node = Rc::clone(&root);
                 continue;
@@ -42,37 +41,28 @@ fn main() -> Result<(), Box<dyn Error>> {
                 state = State::ParsePattern;
                 continue;
             }
-            // (b'\n', State::InvalidPattern) => {
-            //     println!("S");
-            //     current_node = Rc::clone(&root);
-            //     current_nodes0.clear();
-            //     current_nodes0.push(Rc::clone(&root));
-            //     current_nodes1.clear();
-            //     state = State::ParsePattern;
-            //     let mut buffer = String::new();
-            //     io::stdin().read_line(&mut buffer)?;
-
-            //     continue;
-            // }
-            (b'\n', State::ParsePattern) => {
-                // println!(
-                //     "_ {} {} {:#?}",
-                //     counter,
-                //     current_node.borrow().towel,
-                //     current_nodes0.iter().filter(|v| v.borrow().towel).count()
-                // );
-                if current_nodes0.iter().filter(|v| v.borrow().towel).count() > 0 {
-                    counter += 1;
-                } else {
-                    println!("FAIL");
-                    let mut buffer = String::new();
-                    io::stdin().read_line(&mut buffer)?;
-                }
+            (b'\n', State::InvalidPattern) => {
                 current_node = Rc::clone(&root);
                 current_nodes0.clear();
                 current_nodes1.clear();
-                current_nodes0.push(Rc::clone(&root));
-                // break;
+                current_nodes0.push((1, Rc::clone(&root)));
+                state = State::ParsePattern;
+                continue;
+            }
+            (_, State::InvalidPattern) => continue,
+            (b'\n', State::ParsePattern) => {
+                let ways = current_nodes0
+                    .iter()
+                    .filter(|(_, v)| v.borrow().towel)
+                    .fold(0, |acc, (c, _)| acc + c);
+                if ways > 0 {
+                    solution1 += 1;
+                }
+                solution2 += ways;
+                current_node = Rc::clone(&root);
+                current_nodes0.clear();
+                current_nodes1.clear();
+                current_nodes0.push((1, Rc::clone(&root)));
                 continue;
             }
             (b' ', _) => continue,
@@ -83,7 +73,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             (b'g', _) => 4,
             _ => panic!("unexpected value: {}", str::from_utf8(&[v]).unwrap()),
         };
-        print!("{} ", str::from_utf8(&[v]).unwrap());
+        // print!("{} ", str::from_utf8(&[v]).unwrap());
 
         if state == State::ParseTree {
             current_node = {
@@ -99,34 +89,37 @@ fn main() -> Result<(), Box<dyn Error>> {
                 next_node
             };
         } else {
-            print!("{index} ");
-
             (current_nodes0, current_nodes1) = (current_nodes1, current_nodes0);
-
+            let mut towels = 0;
             while let Some(current_node) = current_nodes1.pop() {
-                let b = current_node.borrow();
+                let b = current_node.1.borrow();
 
                 if let Some(next) = &b.next[index] {
-                    // println!("MATCH {} {}", b.towel, next.borrow().towel);
-                    current_nodes0.push(Rc::clone(next));
-                    // print!("- ");
+                    current_nodes0.push((current_node.0, Rc::clone(next)));
                 }
-                // println!("NO MATCH {}", b.towel);
                 if b.towel {
-                    let b = root.borrow();
-                    if let Some(next) = &b.next[index] {
-                        // println!("MATCH2 {} {}", b.towel, next.borrow().towel);
-                        current_nodes0.push(Rc::clone(next));
-                        // print!("+ ");
-                    }
+                    towels += current_node.0;
                 }
             }
-            println!("{} {}", current_nodes0.len(), current_nodes1.len());
-            // let mut buffer = String::new();
-            // io::stdin().read_line(&mut buffer)?;
+
+            if towels > 0 {
+                let b = root.borrow();
+                if let Some(next) = &b.next[index] {
+                    current_nodes0.push((towels, Rc::clone(next)));
+                }
+            }
+
+            if current_nodes0.is_empty() {
+                state = State::InvalidPattern;
+            }
         }
     }
-    println!("Solution: {} / Duration: {:.6?}", counter, t0.elapsed());
+    println!(
+        "Solution: {} {} / Duration: {:.6?}",
+        solution1,
+        solution2,
+        t0.elapsed()
+    );
     Ok(())
 }
 
@@ -143,6 +136,4 @@ impl Trie {
             next: [const { None }; 5],
         }
     }
-
-    // fn get_next(&mut self, )
 }
