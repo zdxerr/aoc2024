3899c5959413eb8ba8b651a7d9fb2ddcd9173e1d
Christoph Schniedermeier <CSchniedermeier@dspace.de>
2025-02-24 17:35:47 +0100

    Day 20.1 solution.

--- day201/src/main.rs
+++ day201/src/main.rs
@@ -1,9 +1,9 @@
 use core::panic;
+use std::collections::HashMap;
 use std::error::Error;
 use std::time::Instant;
 use std::{env, fs, str};
 
-const WAY: u8 = b'.';
 const WALL: u8 = b'#';
 const START: u8 = b'S';
 const END: u8 = b'E';
@@ -16,44 +16,58 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Read {input_path}.");
 
     let xlen = grid.iter().position(|v| *v == NL).unwrap();
-    let mut pos = grid.iter().position(|v| *v == START).unwrap();
-
-    let mut times: Vec<u64> = vec![u64::MAX; grid.len()];
-
+    let start = grid.iter().position(|v| *v == START).unwrap();
+    let mut times = vec![usize::MAX; grid.len()];
+    let mut jumps: Vec<(usize, usize)> = vec![];
+    let mut pos = start;
     times[pos] = 0;
 
-    while grid[pos] != END {
+    loop {
         let (n, e, s, w) = (pos - xlen - 1, pos + 1, pos + xlen + 1, pos - 1);
+        let (e2, s2) = (pos + 2, pos + 2 * (xlen + 1));
 
-        let directions = (
-            grid[n] == WAY,
-            grid[e] == WAY,
-            grid[s] == WAY,
-            grid[w] == WAY,
-        );
+        jumps.extend([(pos, e2), (pos, s2)]);
 
-        let c = (times[pos], times[n], times[e], times[s], times[w]);
-        println!(
-            "{xlen} {pos} {} {n} {e} {s} {w} {directions:#?} {c:#?}",
-            grid[pos] as char
-        );
-
-        if grid[n] == WAY && times[n] > times[pos] {
+        if grid[pos] == END {
+            break;
+        }
+        if grid[n] != WALL && times[n] > times[pos] {
             times[n] = times[pos] + 1;
+
             pos = n;
-        } else if grid[e] == WAY && times[e] > times[pos] {
+        } else if grid[e] != WALL && times[e] > times[pos] {
             times[e] = times[pos] + 1;
             pos = e;
-        } else if grid[s] == WAY && times[s] > times[pos] {
+        } else if grid[s] != WALL && times[s] > times[pos] {
             times[s] = times[pos] + 1;
             pos = s;
-        } else if grid[w] == WAY && times[w] > times[pos] {
+        } else if grid[w] != WALL && times[w] > times[pos] {
             times[w] = times[pos] + 1;
             pos = w;
         } else {
             panic!("Something went wrong.");
         }
     }
-    println!("Solution: {} / Duration: {:.6?}", 0, t0.elapsed());
+    // let mut hm: HashMap<usize, usize> = HashMap::new();
+    let mut solution = 0_usize;
+    for (a, b) in jumps {
+        match grid.get(b) {
+            Some(&WALL | &NL) | None => continue,
+            _ => {}
+        }
+        let x = times[b].abs_diff(times[a]).saturating_sub(2);
+        // if x > 0 {
+        //     hm.entry(x).and_modify(|v| *v += 1).or_insert(1);
+        //     // println!(
+        //     //     "JUMP {} {a}({}) -> {b}({}) {}",
+        //     //     grid[b] as char, times[a], times[b], x,
+        //     // );
+        // }
+        if x >= 100 {
+            solution += 1;
+        }
+    }
+    // println!("{hm:#?}");
+    println!("Solution: {} / Duration: {:.6?}", solution, t0.elapsed());
     Ok(())
 }
