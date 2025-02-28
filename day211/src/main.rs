3ba129352878695a0fd5c50ccbaff9e710ff6e56
Christoph Schniedermeier <CSchniedermeier@dspace.de>
2025-02-28 16:25:03 +0100

    Day 21.1 numpad combinations.

--- day211/src/main.rs
+++ day211/src/main.rs
@@ -1,6 +1,9 @@
+use core::str;
 use std::error::Error;
 use std::time::Instant;
 use std::{env, fs};
+
+use std::collections::HashMap;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let t0 = Instant::now();
@@ -29,7 +32,66 @@ fn main() -> Result<(), Box<dyn Error>> {
         .flatten()
         .collect();
 
-    dbg!(numcoords, dcoords);
+    let mut padpaths: HashMap<u8, HashMap<u8, Vec<Vec<u8>>>> = HashMap::new();
+
+    let start = (2_usize, 3_usize);
+    // let next = (start.0, start.1 - 1);
+
+    let mut stack = vec![(numpad[start.1][start.0], 2_usize, 3_usize, vec![])];
+
+    while let Some((start, x, y, path)) = stack.pop() {
+        // dbg!(start, x, y, &path);c
+        if x > 0 {
+            stack.push((
+                start,
+                x - 1,
+                y,
+                path.iter().copied().chain([b'<'].into_iter()).collect(),
+            ));
+            if start != numpad[y][x] {
+                stack.push((numpad[y][x], x - 1, y, vec![b'<']));
+            }
+        }
+        if y > 0 {
+            stack.push((
+                start,
+                x,
+                y - 1,
+                path.iter().copied().chain([b'^'].into_iter()).collect(),
+            ));
+            if start != numpad[y][x] {
+                stack.push((numpad[y][x], x, y - 1, vec![b'^']));
+            }
+        }
+        // dbg!(start, x, y, &path);
+        println!(
+            "{} {} {}",
+            start as char,
+            numpad[y][x] as char,
+            str::from_utf8(&path)?
+        );
+        padpaths
+            .entry(start)
+            .or_default()
+            .entry(numpad[y][x])
+            .or_default()
+            .push(path);
+        // break;
+    }
+
+    // while pos != (0, 0) {}
+
+    // dbg!(numcoords, dcoords, &padpaths, &padpaths.len());
+
+    for (a, p) in padpaths {
+        for (b, paths) in p {
+            println!("{} -> {}", a as char, b as char);
+
+            for path in paths {
+                println!("    {}", str::from_utf8(&path)?);
+            }
+        }
+    }
 
     println!("Solution: {} / Duration: {:.6?}", 0, t0.elapsed());
     Ok(())
