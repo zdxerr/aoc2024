e77a7fe005c38b4d5b6bb9ecf48e956522e71097
Christoph Schniedermeier <zdxerr@gmail.com>
2025-03-04 19:16:01 +0100

    Day 21.1 21.2 solutions.

--- day211/src/main.rs
+++ day211/src/main.rs
@@ -1,6 +1,5 @@
 use core::str;
 use std::error::Error;
-use std::f32::consts;
 use std::iter::repeat_n;
 use std::time::Instant;
 use std::{env, fs};
@@ -25,53 +24,56 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
     compute_padpaths(&mut padpaths, &[[b' ', b'^', b'A'], [b'<', b'v', b'>']]);
 
+    let mut cache: HashMap<(u8, u8, usize), usize> = HashMap::new();
+
     let mut solution = 0_usize;
     for code in input.trim_ascii().split(|c| *c == b'\n') {
         let number: usize = str::from_utf8(code.split_last_chunk::<1>().unwrap().0)?.parse()?;
-        let length = solve(code, &padpaths, 2);
+        let length = solve(code, &padpaths, 25, &mut cache);
         let complexity = number * length;
-        println!("{} * {} = {}", length, number, complexity);
+        // println!("{} * {} = {}", length, number, complexity);
 
         solution += complexity;
-
-        // break;
     }
 
     println!("Solution: {} / Duration: {:.6?}", solution, t0.elapsed());
     Ok(())
 }
 
-fn solve(code: &[u8], padpaths: &HashMap<u8, HashMap<u8, Vec<Vec<u8>>>>, depth: usize) -> usize {
-    // println!("{} depth={}", str::from_utf8(code).unwrap(), depth);
+fn solve(
+    code: &[u8],
+    padpaths: &HashMap<(u8, u8), Vec<Vec<u8>>>,
+    depth: usize,
+    cache: &mut HashMap<(u8, u8, usize), usize>,
+) -> usize {
     let mut pos = b'A';
     let mut sum = 0_usize;
     for next in code {
         if depth == 0 {
-            sum += padpaths
-                .get(&pos)
-                .unwrap()
-                .get(next)
-                .unwrap()
-                .get(0)
-                .unwrap()
-                .len();
+            sum += padpaths.get(&(pos, *next)).unwrap().get(0).unwrap().len();
         } else {
-            sum += padpaths
-                .get(&pos)
-                .unwrap()
-                .get(&next)
-                .unwrap()
-                .iter()
-                .map(|path| solve(path, padpaths, depth - 1))
-                .min()
-                .unwrap();
+            let key = (pos, *next, depth - 1);
+            sum += if cache.contains_key(&key) {
+                *cache.get(&key).unwrap()
+            } else {
+                let len = padpaths
+                    .get(&(pos, *next))
+                    .unwrap()
+                    .iter()
+                    .map(|path| solve(path, padpaths, depth - 1, cache))
+                    .min()
+                    .unwrap();
+                cache.insert(key, len);
+                len
+            };
         }
+
         pos = *next;
     }
     sum
 }
 
-fn compute_padpaths(padpaths: &mut HashMap<u8, HashMap<u8, Vec<Vec<u8>>>>, pad: &[[u8; 3]]) {
+fn compute_padpaths(padpaths: &mut HashMap<(u8, u8), Vec<Vec<u8>>>, pad: &[[u8; 3]]) {
     let stack: Vec<(u8, usize, usize)> = pad
         .iter()
         .enumerate()
@@ -85,14 +87,14 @@ fn compute_padpaths(padpaths: &mut HashMap<u8, HashMap<u8, Vec<Vec<u8>>>>, pad: 
 
     for (from, x0, y0) in &stack {
         for (to, x1, y1) in &stack {
-            let paths = padpaths.entry(*from).or_default().entry(*to).or_default();
+            let paths = padpaths.entry((*from, *to)).or_default();
             if from == to {
                 paths.push(vec![b'A']);
                 continue;
             }
             let path: Vec<u8> = repeat_n(if x0 < x1 { b'>' } else { b'<' }, x1.abs_diff(*x0))
                 .chain(repeat_n(
-                    if y0 < y1 { b'^' } else { b'v' },
+                    if y0 < y1 { b'v' } else { b'^' },
                     y1.abs_diff(*y0),
                 ))
                 .collect();
