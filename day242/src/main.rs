b8fc0f10b4724303bb82614fa8ad0eb5c0d976ff
Christoph Schniedermeier <CSchniedermeier@dspace.de>
2025-03-14 15:55:48 +0100

    Solution attempt for day 24.2.

--- day242/src/main.rs
+++ day242/src/main.rs
@@ -1,9 +1,10 @@
-use regex::{Match, Regex};
-use std::collections::HashMap;
+use regex::Regex;
+use std::collections::BTreeMap;
 use std::error::Error;
 use std::time::Instant;
 use std::{env, fs};
 
+#[allow(clippy::too_many_lines)]
 fn main() -> Result<(), Box<dyn Error>> {
     let t0 = Instant::now();
     let input_path = env::args().nth(1).expect("no input path");
@@ -14,7 +15,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         r"(?:([a-z0-9]+)\s*:\s*([01]))|(?:([a-z0-9]+)\s+((?:AND)|(?:OR)|(?:XOR))\s+([a-z0-9]+)\s*->\s*([a-z0-9]+))",
     )
     .unwrap();
-    let map: HashMap<String, Expression> = expr
+    let map: BTreeMap<String, Expression> = expr
         .captures_iter(input.as_str())
         .map(|cap| {
             if let (Some(port), Some(value)) = (cap.get(1), cap.get(2)) {
@@ -44,40 +45,180 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .collect();
 
-    fn get_ports(prefix: char, map: &HashMap<String, Expression>) -> Vec<&String> {
-        let mut s: Vec<&String> = map.keys().filter(|k| k.starts_with(prefix)).collect();
-        s.sort();
-        s.reverse();
-        s
+    let mut outputs: Vec<&str> = vec![];
+
+    for (z, expr) in map.iter().filter(|(k, _)| k.starts_with('z')) {
+        let number: u32 = z[1..].parse().unwrap();
+        // println!("{number} {z}: {expr:?}");
+        if let Expression::Gate { a, op, b } = expr {
+            if *op != Operator::XOR {
+                outputs.push(z);
+                continue;
+            }
+            if number == 0 {
+                if !([a, b] == ["x00", "y00"] || [b, a] == ["x00", "y00"]) {
+                    outputs.push(z);
+                }
+                continue;
+            }
+
+            let mut x_xor_y = false;
+            let inputs = [&format!("x{number:02}"), &format!("y{number:02}")];
+            match (a, map.get(a).unwrap(), b, map.get(b).unwrap()) {
+                (
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: Operator::XOR,
+                        b: b1,
+                    },
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: Operator::OR,
+                        b: b2,
+                    },
+                )
+                | (
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: Operator::OR,
+                        b: b2,
+                    },
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: Operator::XOR,
+                        b: b1,
+                    },
+                ) if [a1, b1] == inputs || [b1, a1] == inputs => {
+                    println!("{z} / {out2} = {a2}  {b2}");
+
+                    // if number == 1 {
+                    //     if !(*op2 == Operator::AND
+                    //         && ([a2, b2] == ["x00", "y00"] || [b2, a2] == ["x00", "y00"]))
+                    //     {
+                    //         outputs.push(z);
+                    //     }
+                    //     continue;
+                    // }
+                }
+                (
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: Operator::XOR,
+                        b: b1,
+                    },
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: Operator::OR,
+                        b: b2,
+                    },
+                )
+                | (
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: Operator::OR,
+                        b: b2,
+                    },
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: Operator::XOR,
+                        b: b1,
+                    },
+                ) => outputs.push(out1),
+                (
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: _,
+                        b: b1,
+                    },
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: Operator::OR,
+                        b: b2,
+                    },
+                )
+                | (
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: Operator::OR,
+                        b: b2,
+                    },
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: _,
+                        b: b1,
+                    },
+                ) => outputs.push(out1),
+                (
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: Operator::XOR,
+                        b: b1,
+                    },
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: _,
+                        b: b2,
+                    },
+                )
+                | (
+                    out2,
+                    Expression::Gate {
+                        a: a2,
+                        op: _,
+                        b: b2,
+                    },
+                    out1,
+                    Expression::Gate {
+                        a: a1,
+                        op: Operator::XOR,
+                        b: b1,
+                    },
+                ) => outputs.push(out2),
+                _ => panic!(),
+            }
+        }
+
+        // if let Expression::Gate { a, op, b } = expr {
+        //     let expra = map.get(a).unwrap();
+        //     println!("  - {a}: {expra:?}");
+        //     if let Expression::Gate { a, op, b } = expra {
+        //         let expra = map.get(a).unwrap();
+        //         println!("    - {a}: {expra:?}");
+        //         let exprb = map.get(b).unwrap();
+        //         println!("    - {b}: {exprb:?}");
+        //     y}
+        //     let exprb = map.get(b).unwrap();
+        //     println!("  - {b}: {exprb:?}");
+        //     if let Expression::Gate { a, op, b } = exprb {
+        //         let expra = map.get(a).unwrap();
+        //         println!("    - {a}: {expra:?}");
+        //         let exprb = map.get(b).unwrap();
+        //         println!("    - {b}: {exprb:?}");
+        //     }
+        // }
     }
 
-    let xs = get_ports('x', &map);
-    let ys = get_ports('y', &map);
-    let zs = get_ports('z', &map);
+    outputs.sort_unstable();
 
-    for x in &xs {
-        println!("{x}: {:?}", map[*x]);
-    }
-    for x in &ys {
-        println!("{x}: {:?}", map[*x]);
-    }
-    for x in &zs {
-        println!("{x}: {:?}", map[*x]);
-    }
-
-    let (x, y) = (
-        to_u64(&xs.into_iter().map(|k| map.get(k).unwrap().into()).collect()),
-        to_u64(&ys.into_iter().map(|k| map.get(k).unwrap().into()).collect()),
+    println!(
+        "Solution: {} / Duration: {:.6?}",
+        outputs.join(","),
+        t0.elapsed()
     );
-    let z = x + y;
-
-    println!("X: {} {:#020b}", x, x);
-    println!("Y: {} {:#020b}", y, y);
-    println!("Z: {} {:#020b}", z, z);
-
-    let solution = 0;
-
-    println!("Solution: {} / Duration: {:.6?}", solution, t0.elapsed());
 
     Ok(())
 }
@@ -97,30 +238,9 @@ impl Into<bool> for &Expression {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 enum Operator {
     AND,
     OR,
     XOR,
 }
-
-fn solve(port: &String, map: &HashMap<String, Expression>) -> bool {
-    match map.get(port).unwrap() {
-        Expression::Gate { a, op, b } => {
-            let a = solve(a, map);
-            let b = solve(b, map);
-            match op {
-                Operator::AND => a & b,
-                Operator::OR => a | b,
-                Operator::XOR => a ^ b,
-            }
-        }
-        Expression::Value(value) => *value,
-    }
-}
-
-fn to_u64(values: &Vec<bool>) -> u64 {
-    values.iter().fold(0, |acc, &v| (acc << 1) | u64::from(v))
-}
-
-// fn from_vec()
