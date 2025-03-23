9a7f91a30ba99b5b5ff2707c9c1e040f2c2e049c
Christoph Schniedermeier <zdxerr@gmail.com>
2025-03-23 20:49:12 +0100

    Day 25.1 solution.

--- day251/src/main.rs
+++ day251/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::time::Instant;
+use std::{env, fs};
+
+#[allow(clippy::too_many_lines)]
+fn main() -> Result<(), Box<dyn Error>> {
+    let t0 = Instant::now();
+    let input_path = env::args().nth(1).expect("no input path");
+
+    let input = fs::read(&input_path)?;
+    println!("Read {input_path}.");
+
+    let mut locks: Vec<[u8; 5]> = Vec::new();
+    let mut keys: Vec<[u8; 5]> = Vec::new();
+
+    let mut is_lock: Option<bool> = None;
+    let mut current = [0_u8; 5];
+    for line in input.split(|c| *c == b'\n') {
+        if is_lock.is_none() {
+            is_lock = Some(line == [b'#'; 5]);
+            continue;
+        }
+        if line.is_empty() {
+            if let Some(true) = is_lock {
+                locks.push(current);
+            } else {
+                keys.push(current.map(|c| c - 1));
+            }
+            is_lock = None;
+            current = [0_u8; 5];
+        }
+        for (index, column) in line.iter().enumerate() {
+            if *column == b'#' {
+                current[index] += 1;
+            }
+        }
+    }
+    let mut solution = 0_usize;
+    for lock in &locks {
+        for key in &keys {
+            if lock.iter().zip(key).all(|(a, b)| a + b <= 5) {
+                solution += 1;
+            }
+        }
+    }
+    println!("Solution: {} / Duration: {:.6?}", solution, t0.elapsed());
+
+    Ok(())
 }
