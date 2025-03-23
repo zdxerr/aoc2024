79986603c92c32bf2b0c58d8e9ea8ace4f25f1f5
Christoph Schniedermeier <zdxerr@gmail.com>
2025-03-23 18:57:01 +0100

    Day 24.2 solution.

--- day242/src/main.rs
+++ day242/src/main.rs
@@ -1,262 +1,158 @@
-use regex::Regex;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::error::Error;
 use std::time::Instant;
-use std::{env, fs};
+use std::{env, fs, str};
 
 #[allow(clippy::too_many_lines)]
 fn main() -> Result<(), Box<dyn Error>> {
     let t0 = Instant::now();
     let input_path = env::args().nth(1).expect("no input path");
-    let input = fs::read_to_string(&input_path)?;
-    println!("Read {input_path}.");
-
-    let expr = Regex::new(
-        r"(?:([a-z0-9]+)\s*:\s*([01]))|(?:([a-z0-9]+)\s+((?:AND)|(?:OR)|(?:XOR))\s+([a-z0-9]+)\s*->\s*([a-z0-9]+))",
-    )
-    .unwrap();
-    let map: BTreeMap<String, Expression> = expr
-        .captures_iter(input.as_str())
-        .map(|cap| {
-            if let (Some(port), Some(value)) = (cap.get(1), cap.get(2)) {
-                (
-                    port.as_str().to_string(),
-                    Expression::Value(value.as_str() == "1"),
-                )
-            } else if let (Some(in0), Some(op), Some(in1), Some(out)) =
-                (cap.get(3), cap.get(4), cap.get(5), cap.get(6))
-            {
-                (
-                    out.as_str().to_string(),
-                    Expression::Gate {
-                        a: in0.as_str().to_string(),
-                        op: match op.as_str() {
-                            "AND" => Operator::AND,
-                            "OR" => Operator::OR,
-                            "XOR" => Operator::XOR,
-                            _ => panic!("unknown operator"),
-                        },
-                        b: in1.as_str().to_string(),
-                    },
-                )
-            } else {
-                panic!();
-            }
-        })
-        .collect();
 
     let mut outputs: Vec<&str> = vec![];
 
-    while let Some((out, Expression::Gate { a, op, b })) = map.iter().next() {
-        match *op {
-            Operator::XOR => {
-                if out == "z00" && ([a, b] == ["x00", "y00"] || [b, a] == ["x00", "y00"]) {
-                    continue;
-                }
-                if [a[0], b[0]] == [x, y] || [a[0], b[0]] == [y, x] {
+    let input = fs::read_to_string(&input_path)?;
+    println!("Read {input_path}.");
 
-                    // map.get(out)
-                }
+    let (_, gates) = input.split_once("\n\n").unwrap();
+
+    let mut inports: HashMap<[u8; 3], &[u8]> = HashMap::new();
+    let gates: HashMap<[u8; 3], ([u8; 3], &[u8], [u8; 3])> = gates
+        .trim()
+        .lines()
+        .map(|line| {
+            let mut parts = line.split_ascii_whitespace();
+            [
+                parts.next().unwrap().as_bytes(),
+                parts.next().unwrap().as_bytes(),
+                parts.next().unwrap().as_bytes(),
+                parts.next().unwrap().as_bytes(),
+                parts.next().unwrap().as_bytes(),
+            ]
+        })
+        .map(|[a, op, b, _, c]| {
+            inports.insert(a.try_into().unwrap(), op);
+            inports.insert(b.try_into().unwrap(), op);
+            (
+                c.try_into().unwrap(),
+                (a.try_into().unwrap(), op, b.try_into().unwrap()),
+            )
+        })
+        .collect();
+
+    for (c, (a, op, b)) in &gates {
+        match (a, gates.get(a), op, b, gates.get(b), c, inports.get(c)) {
+            (
+                [b'x', b'0', b'0'],
+                _,
+                [b'X', b'O', b'R'],
+                [b'y', b'0', b'0'],
+                _,
+                [b'z', b'0', b'0'],
+                _,
+            )
+            | (
+                [b'y', b'0', b'0'],
+                _,
+                [b'X', b'O', b'R'],
+                [b'x', b'0', b'0'],
+                _,
+                [b'z', b'0', b'0'],
+                _,
+            ) => {}
+            (
+                [b'x', b'0', b'0'],
+                _,
+                [b'A', b'N', b'D'],
+                [b'y', b'0', b'0'],
+                _,
+                _,
+                Some([b'X', b'O', b'R']),
+            )
+            | (
+                [b'y', b'0', b'0'],
+                _,
+                [b'A', b'N', b'D'],
+                [b'x', b'0', b'0'],
+                _,
+                _,
+                Some([b'X', b'O', b'R']),
+            ) => {}
+            (
+                [b'x', x1, x0],
+                _,
+                [b'X', b'O', b'R'],
+                [b'y', y1, y0],
+                _,
+                _,
+                Some([b'X', b'O', b'R']) | Some([b'A', b'N', b'D']),
+            ) if x1 == y1 && x0 == y0 => {}
+            (
+                [b'y', y1, y0],
+                _,
+                [b'X', b'O', b'R'],
+                [b'x', x1, x0],
+                _,
+                _,
+                Some([b'X', b'O', b'R']) | Some([b'A', b'N', b'D']),
+            ) if x1 == y1 && x0 == y0 => {}
+            (_, _, [b'X', b'O', b'R'], _, _, [b'z', _, _], _) => {}
+            ([b'x', _, _], _, [b'A', b'N', b'D'], [b'y', _, _], _, _, Some([b'O', b'R'])) => {}
+            ([b'y', _, _], _, [b'A', b'N', b'D'], [b'x', _, _], _, _, Some([b'O', b'R'])) => {}
+            (
+                _,
+                Some((_, [b'O', b'R'], _)),
+                [b'A', b'N', b'D'],
+                _,
+                Some((_, [b'X', b'O', b'R'], _)),
+                _,
+                Some([b'O', b'R']),
+            ) => {}
+            ([b'y', _, _], _, [b'A', b'N', b'D'], [b'x', _, _], _, _, Some([b'X', b'O', b'R'])) => {
             }
-            Operator::OR => {}
-            Operator::AND => {}
+            (_, _, [b'A', b'N', b'D'], _, _, _, Some([b'O', b'R'])) => {}
+            (_, _, [b'O', b'R'], _, _, _, Some([b'X', b'O', b'R']) | Some([b'A', b'N', b'D'])) => {}
+            (
+                _,
+                Some((_, [b'A', b'N', b'D'], _)),
+                [b'O', b'R'],
+                _,
+                Some((_, [b'A', b'N', b'D'], _)),
+                [b'z', b'4', b'5'],
+                _,
+            ) => {}
+            _ => outputs.push(str::from_utf8(c).unwrap()),
         }
     }
 
-    // for (z, expr) in map.iter().filter(|(k, _)| k.starts_with('z')) {
-    //     let number: u32 = z[1..].parse().unwrap();
-    //     // println!("{number} {z}: {expr:?}");
-    //     if let Expression::Gate { a, op, b } = expr {
-    //         if *op != Operator::XOR {
-    //             outputs.push(z);
-    //             continue;
-    //         }
-    //         if number == 0 {
-    //             if !([a, b] == ["x00", "y00"] || [b, a] == ["x00", "y00"]) {
-    //                 outputs.push(z);
-    //             }
-    //             continue;
-    //         }
-
-    //         let mut x_xor_y = false;
-    //         let inputs = [&format!("x{number:02}"), &format!("y{number:02}")];
-    //         match (a, map.get(a).unwrap(), b, map.get(b).unwrap()) {
-    //             (
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: Operator::XOR,
-    //                     b: b1,
-    //                 },
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: Operator::OR,
-    //                     b: b2,
-    //                 },
-    //             )
-    //             | (
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: Operator::OR,
-    //                     b: b2,
-    //                 },
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: Operator::XOR,
-    //                     b: b1,
-    //                 },
-    //             ) if [a1, b1] == inputs || [b1, a1] == inputs => {
-    //                 println!("{z} / {out2} = {a2}  {b2}");
-
-    //                 // if number == 1 {
-    //                 //     if !(*op2 == Operator::AND
-    //                 //         && ([a2, b2] == ["x00", "y00"] || [b2, a2] == ["x00", "y00"]))
-    //                 //     {
-    //                 //         outputs.push(z);
-    //                 //     }
-    //                 //     continue;
-    //                 // }
-    //             }
-    //             (
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: Operator::XOR,
-    //                     b: b1,
-    //                 },
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: Operator::OR,
-    //                     b: b2,
-    //                 },
-    //             )
-    //             | (
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: Operator::OR,
-    //                     b: b2,
-    //                 },
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: Operator::XOR,
-    //                     b: b1,
-    //                 },
-    //             ) => outputs.push(out1),
-    //             (
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: _,
-    //                     b: b1,
-    //                 },
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: Operator::OR,
-    //                     b: b2,
-    //                 },
-    //             )
-    //             | (
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: Operator::OR,
-    //                     b: b2,
-    //                 },
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: _,
-    //                     b: b1,
-    //                 },
-    //             ) => outputs.push(out1),
-    //             (
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: Operator::XOR,
-    //                     b: b1,
-    //                 },
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: _,
-    //                     b: b2,
-    //                 },
-    //             )
-    //             | (
-    //                 out2,
-    //                 Expression::Gate {
-    //                     a: a2,
-    //                     op: _,
-    //                     b: b2,
-    //                 },
-    //                 out1,
-    //                 Expression::Gate {
-    //                     a: a1,
-    //                     op: Operator::XOR,
-    //                     b: b1,
-    //                 },
-    //             ) => outputs.push(out2),
-    //             _ => panic!(),
-    //         }
-    //     }
-
-    //     // if let Expression::Gate { a, op, b } = expr {
-    //     //     let expra = map.get(a).unwrap();
-    //     //     println!("  - {a}: {expra:?}");
-    //     //     if let Expression::Gate { a, op, b } = expra {
-    //     //         let expra = map.get(a).unwrap();
-    //     //         println!("    - {a}: {expra:?}");
-    //     //         let exprb = map.get(b).unwrap();
-    //     //         println!("    - {b}: {exprb:?}");
-    //     //     y}
-    //     //     let exprb = map.get(b).unwrap();
-    //     //     println!("  - {b}: {exprb:?}");
-    //     //     if let Expression::Gate { a, op, b } = exprb {
-    //     //         let expra = map.get(a).unwrap();
-    //     //         println!("    - {a}: {expra:?}");
-    //     //         let exprb = map.get(b).unwrap();
-    //     //         println!("    - {b}: {exprb:?}");
-    //     //     }
-    //     // }
-    // }
-
     outputs.sort_unstable();
 
+    for output in &outputs {
+        let gate = gates.get(output.as_bytes()).unwrap();
+        let inport =
+            str::from_utf8(inports.get(output.as_bytes()).unwrap_or(&[b'-'].as_slice())).unwrap();
+
+        let a = gates
+            .get(&gate.0)
+            .unwrap_or(&([b'-'; 3], &[b'-'], [b'-'; 3]));
+        let b = gates
+            .get(&gate.2)
+            .unwrap_or(&([b'-'; 3], &[b'-'], [b'-'; 3]));
+        println!(
+            "{output} ({inport}) = {} ({}) {} {} ({})",
+            str::from_utf8(&gate.0).unwrap(),
+            str::from_utf8(&a.1).unwrap(),
+            str::from_utf8(&gate.1).unwrap(),
+            str::from_utf8(&gate.2).unwrap(),
+            str::from_utf8(&b.1).unwrap(),
+        )
+    }
+
     println!(
-        "Solution: {} / Duration: {:.6?}",
+        "Solution: ({}) {} / Duration: {:.6?}",
+        outputs.len(),
         outputs.join(","),
         t0.elapsed()
     );
 
     Ok(())
-}
-
-#[derive(Debug)]
-enum Expression {
-    Value(bool),
-    Gate { a: String, op: Operator, b: String },
-}
-
-impl Into<bool> for &Expression {
-    fn into(self) -> bool {
-        match self {
-            Expression::Value(value) => *value,
-            _ => panic!(),
-        }
-    }
-}
-
-#[derive(Debug, Eq, PartialEq)]
-enum Operator {
-    AND,
-    OR,
-    XOR,
 }
