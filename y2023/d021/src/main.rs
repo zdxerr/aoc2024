4beeda7854212f2a4fae183a552687fe2974277f
Christoph Schniedermeier <zdxerr@gmail.com>
2025-03-29 12:48:03 +0100

    Add 2023.2.1.

--- y2023/d021/src/main.rs
+++ y2023/d021/src/main.rs
@@ -11,27 +11,28 @@ fn main() -> Result<(), Error> {
     let input_path = env::args().nth(1).expect("no input path");
     let buff_reader = BufReader::new(fs::File::open(input_path)?);
 
-    for line in buff_reader.lines() {
+    let mut solution: u32 = 0;
+
+    'outer: for line in buff_reader.lines() {
         let line = line.expect("unable to read line");
         let (game, rest) = line.split_once(':').unwrap();
         let (_, game) = game.split_once(' ').unwrap();
-        game.parse::<u32>();
 
         for draw in rest.split(';') {
-            for color_and_number in draw.split(",") {
-                let (color, number) = color_and_number.split_once(' ').unwrap();
-
+            for number_and_color in draw.split(",") {
+                let (number, color) = number_and_color.trim().split_once(' ').unwrap();
+                // dbg!(number);
+                let number: u32 = number.parse().unwrap();
                 match color {
-                    "red" => {}
-                    "green" => {}
-                    "blue" => {}
+                    "red" if number > RED => continue 'outer,
+                    "green" if number > GREEN => continue 'outer,
+                    "blue" if number > BLUE => continue 'outer,
                     _ => {}
                 }
-                number.parse::<u32>();
-                println!("{game} {color}");
             }
         }
+        solution += game.parse::<u32>().unwrap();
     }
-    println!("Solution: {} / Duration: {:.6?}", 0, t0.elapsed());
+    println!("Solution: {} / Duration: {:.6?}", solution, t0.elapsed());
     Ok(())
 }
